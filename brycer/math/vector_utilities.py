2d7dc0e87ca542850d4b1a54c30a2fe2c34c2f6a
Rob W <robkwatson1@gmail.com>
2020-12-04 20:01:12 -0500

    clean-up of vector_utilities (1st pass)

--- brycer/math/vector_utilities.py
+++ brycer/math/vector_utilities.py
@@ -2,34 +2,55 @@ from pygame.math import Vector2
 from brycer.math import math_utilities
 
 
-def calc_vector(magnitude, angle):
+def calc_vector(magnitude: [int, float], angle: [int, float]) -> Vector2:
+    """
+    :param magnitude - length of the new vector
+    :param angle (in polar coordinates) of the new vector
+    :return pygame.math.Vector2 object
+    Creates a vector object with the given magnitude and angle
+    """
     return Vector2(magnitude, 0).rotate(angle)
 
 
-def truncate(value, limit):
+def truncate(value: Vector2, maximum: [int, float]) -> Vector2:
     """
-    truncate function for vectors
+    :param value - vector to be truncated
+    :param maximum - vector length limit
+    :return pygame.math.Vector2 object
+    Returns a vector object that is limited a given length.
+    Vector will inherit the angle of the given value vector.
     """
-    return value if value.length() < limit.length() else limit
+    if value.length() > maximum:
+        value.scale_to_length(maximum)
+    return value
 
 
-def at_least(value, floor):
+def at_least(value: Vector2, minimum: [int, float]) -> Vector2:
     """
-    at least function for vectors
+    :param value - vector to be truncated
+    :param minimum - vector minimum length
+    :return pygame.math.Vector2 object
+    Returns a vector object that has at least the given length.
+    Vector will inherit the angle of the given value vector.
     """
-    return value if value.length() > floor.length() else floor
+    if value.length() < minimum:
+        value.scale_to_length(minimum)
+    return value
 
 
-def clamp(value, minimum, maximum):
+def clamp(value: Vector2, minimum: [int, float], maximum: [int, float]) -> Vector2:
     """
-    Clamp function for vectors
+    :param value - vector to be truncated
+    :param minimum - vector minimum length
+    :param maximum - vector maximum length
+    :return pygame.math.Vector2 object
+    Returns a vector object that is between the minimum and maximum lengths.
+    Vector will inherit the angle of the given value vector.
     """
-    if minimum.length() < maximum.length():
-        smallest = minimum
-        largest = maximum
+    if minimum < maximum:
+        smallest, largest = minimum, maximum
     else:
-        smallest = maximum
-        largest = minimum
+        smallest, largest = maximum, minimum
     return truncate(at_least(value, smallest), largest)
 
 
@@ -38,13 +59,11 @@ def limit_rotation(current, desired, rotation_limit) -> Vector2:
     Limit a vector angle change to within value +/- of rotatin_limit
     """
     magnitude, angle = current.as_polar()
-    print(angle)
     return calc_vector(magnitude, math_utilities.clamp(angle + desired, angle - rotation_limit, angle + rotation_limit))
 
 
 def compass(angle):
     return angle - 90 if angle > 90 else (angle + 270) % 360
-    # return (270 - angle) % 360
 
 
 def to_polar(angle):
@@ -52,7 +71,24 @@ def to_polar(angle):
     return (angle - 270) if angle > 90 else (angle + 90)
 
 
+def as_integer(vector: Vector2):
+    _x, _y = vector.xy
+    return Vector2((int(round(_x, 0)), int(round(_y, 0))))
+
+
 if __name__ == '__main__':
+    # testing
     print()
-    v1 = Vector2()
-    print(to_polar(0))
+    v1 = Vector2(191, 0)
+    lower_limit = Vector2(200, 0)
+    upper_limit = Vector2(500, 0)
+
+    v1.rotate_ip(45)
+    lower_limit.rotate_ip(0)
+    upper_limit.rotate_ip(0)
+    print("value=", v1, v1.length(), v1.as_polar()[1])
+    print("lower_limit=", lower_limit, lower_limit.length(), lower_limit.as_polar()[1])
+    print("upper_limit=", upper_limit, upper_limit.length(), upper_limit.as_polar()[1])
+    v2 = clamp(as_integer(v1), lower_limit, upper_limit)
+    print("v2=", v2, v2.length(), v2.as_polar()[1])
+    print(as_integer(v2))
